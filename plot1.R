3c3c2fa66517e3936d851300bb9a3694551e99af
rayyanzuberi <zuberirayyan@gmail.com>
2022-11-19 02:22:43 -0700

    Commit all files, plots, and scripts

---
+++ plot1.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(lubridate)
+
+#check if file exists, and if not, download and unzip
+if(file.exists('hpc.zip')) {
+  print('file present')
+} else {
+  download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', 'hpc.zip')
+  unzip('hpc.zip')
+}
+
+#read table into memory
+hpc <- read.table('household_power_consumption.txt', sep=';', header=TRUE, na.strings = '?')
+
+#convert date and time to datetime
+hpc <- hpc %>%
+  mutate(datetime = dmy(Date) + hms(Time))
+
+#filter using dates for all times between the first and the end of the second of Feb 2007
+hpc0102 <- hpc %>% 
+  filter(datetime %between% c('2007-02-01', '2007-02-03')) %>%
+  slice(1:(n() - 1))
+
+#open device and set parameters
+png(filename = './plot1.png', width = 480, height = 480, units='px')
+
+#create histogram
+hist(hpc0102$Global_active_power, col='red', xlab='Global Active Power (kiloWatts)', main='Global Active Power')
+
+#exit device
+dev.off()
+
+
+
---
+++ plot3.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(lubridate)
+
+#check if file exists, and if not, download and unzip
+if(file.exists('hpc.zip')) {
+  print('file present')
+} else {
+  download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', 'hpc.zip')
+  unzip('hpc.zip')
+}
+
+#read table into memory
+hpc <- read.table('household_power_consumption.txt', sep=';', header=TRUE, na.strings = '?')
+
+#convert date and time to datetime
+hpc <- hpc %>%
+  mutate(datetime = dmy(Date) + hms(Time))
+
+#filter using dates for all times between the first and the end of the second of Feb 2007
+hpc0102 <- hpc %>% 
+  filter(datetime %between% c('2007-02-01', '2007-02-03')) %>%
+  slice(1:(n() - 1))
+
+#open device and set parameters
+png(filename = './plot3.png', width = 480, height = 480, units='px')
+
+#create plot and customize labels
+with(hpc0102, plot(datetime, Sub_metering_1, ylab = 'Energy sub metering', type='n'))
+with(hpc0102, lines(datetime, Sub_metering_1, col='black'))
+with(hpc0102, lines(datetime, Sub_metering_2, col='red'))
+with(hpc0102, lines(datetime, Sub_metering_3, col='blue'))
+
+#close device
+dev.off()
